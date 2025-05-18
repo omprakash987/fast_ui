173a9ac115a6a3b8733f5ff34c79e1f5579e4d89
omprakash parewa <kanuparewa371@gmail.com>
2025-05-18 17:50:23 +0530

    remove the image import from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import AIInput from "@/components/ui_code_components/ai-input/ai-input";
 import Alert from "@/components/ui_code_components/alert/alert";
 export default function Home() {
